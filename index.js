29a1178e9b92e3df24ad769b2ad546428b8f1846
Eugenia <demichka@gmail.com>
2019-12-30 22:26:14 +0100

    added connection to db

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const connectionDB = require("./config/connectionDB");
 
 const app = express();
-const port = 3000;
-
 app.use(express.json());
+const port = 3000;
+mongoose
+	.connect(connectionDB, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true
+	})
+	.then(
+		() => {
+			global.db = mongoose.connection;
+			console.log("Connected to db");
+		},
+		err => {
+			console.error(err);
+		}
+	);
+
 app.listen(port, () => {
-	console.log("Server is listening to ", port);
+	console.log("Server is listening to", port);
 });
