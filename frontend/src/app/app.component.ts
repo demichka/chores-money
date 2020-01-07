76c0ed77ae80157692216ce024af9e9e846e227a
Eugenia <demichka@gmail.com>
2020-01-07 21:55:24 +0100

    added angular material, materialize, basics sidenav and content

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnDestroy } from "@angular/core";
+import { MediaMatcher } from "@angular/cdk/layout";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'frontend';
+  title = "Chores&Money";
+
+  mobileQuery: MediaQueryList;
+
+  onNotice: boolean = true;
+
+  saveChoice: boolean = false;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    if (window.localStorage.getItem("saveChoice") === null) {
+      window.localStorage.setItem("saveChoice", "false");
+    }
+    this.mobileQuery = media.matchMedia("(max-width: 768px)");
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+    this.onNotice =
+      window.localStorage.getItem("saveChoice") === "false" ? true : false;
+  }
+
+  toggleNotice() {
+    this.onNotice = !this.onNotice;
+    if (this.saveChoice) {
+      window.localStorage.setItem("saveChoice", String(this.saveChoice));
+    }
+  }
+
+  setSaveChoice() {
+    this.saveChoice = !this.saveChoice;
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { DemoMaterialModule } from "./material-module";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
