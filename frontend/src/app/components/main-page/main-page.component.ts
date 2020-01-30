0a833ad0709c147ca7fa0d1f5c84ac279e6ce40d
Eugenia <demichka@gmail.com>
2020-01-30 11:07:26 +0100

    hot fix

--- frontend/src/app/components/main-page/main-page.component.ts
+++ frontend/src/app/components/main-page/main-page.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnDestroy, ChangeDetectorRef, OnInit } from "@angular/core";
-import { MediaMatcher } from "@angular/cdk/layout";
+import { Component, OnInit } from "@angular/core";
 import { User } from "src/app/models/user.model";
 import { AuthService } from "src/app/services/auth.service";
 import { Router } from "@angular/router";
@@ -10,38 +9,20 @@ import { UserService } from "src/app/services/user.service";
     templateUrl: "./main-page.component.html",
     styleUrls: ["./main-page.component.scss"]
 })
-export class MainPageComponent implements OnInit, OnDestroy {
+export class MainPageComponent implements OnInit {
     title = "Chores&Money";
 
-    mobileQuery: MediaQueryList;
-
-    onNotice: boolean = true;
-
-    saveChoice: boolean = false;
-
     user: User;
-
-    private _mobileQueryListener: () => void;
 
     isLoading: boolean = false;
 
     constructor(
-        changeDetectorRef: ChangeDetectorRef,
-        media: MediaMatcher,
+
         private authService: AuthService,
         private router: Router,
         private userService: UserService
     ) {
-        if (window.localStorage.getItem("saveChoice") === null) {
-            window.localStorage.setItem("saveChoice", "false");
-        }
-        this.mobileQuery = media.matchMedia("(max-width: 768px)");
-        this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-        this.mobileQuery.addListener(this._mobileQueryListener);
-        this.onNotice =
-            window.localStorage.getItem("saveChoice") === "false"
-                ? true
-                : false;
+        
     }
 
     ngOnInit() {
@@ -52,21 +33,6 @@ export class MainPageComponent implements OnInit, OnDestroy {
                 this.isLoading = false;
             }
         });
-    }
-
-    toggleNotice() {
-        this.onNotice = !this.onNotice;
-        if (this.saveChoice) {
-            window.localStorage.setItem("saveChoice", String(this.saveChoice));
-        }
-    }
-
-    setSaveChoice() {
-        this.saveChoice = !this.saveChoice;
-    }
-
-    ngOnDestroy(): void {
-        this.mobileQuery.removeListener(this._mobileQueryListener);
     }
 
     logout() {
